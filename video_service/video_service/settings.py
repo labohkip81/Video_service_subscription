5614844b75dd4bfda9406335caccdbfc4422c24b
labohkip81 <labankiplagat81@gmail.com>
2019-11-15 05:52:28 +0300

    added static and media directories.

--- video_service/video_service/settings.py
+++ video_service/video_service/settings.py
@@ -122,3 +122,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_FILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_root'),
+]
+
+VENV_PATH = os.path.join(BASE_DIR)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
+
