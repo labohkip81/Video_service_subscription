f7716ba0990aa42643d8a6f35258c382b3342e00
labohkip81 <labankiplagat81@gmail.com>
2019-11-16 16:20:36 +0300

    added al the three models and made migrations for the same.

--- video_service/memberships/models.py
+++ video_service/memberships/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
 
+
+import stripe
+
 MEMBBERSHIP_CHOICES = (
     ('Enterprise', 'ent'),
     ('Professional', 'pro'),
@@ -25,6 +28,22 @@ class UserMembership(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+#using post save signal
+def post_save_usermembership_create(sender, instance, created, *args, **kwargs):
+    if created:
+        UserMembership.objects.get_or_create(user=instance)
+    user_membership, created = UserMembership.objects.get_or_create(user=instance)
+
+    if user_membership.stripe_customer_id is None or user_membership.stripe_customer_id == '':
+        new_customer_id = stripe.Customer.create(email=instance.email)
+        user_membership.stripe_customer_id = new_customer_id['id']
+        user_membership.save()
+
+post_save.connect(post_save_usermembership_create, sender=settings.AUTH_USER_MODEL)
+
+
 
 class Subscription(models.Model):
      user_membership = models.ForeignKey(UserMembership, on_delete=models.CASCADE)
