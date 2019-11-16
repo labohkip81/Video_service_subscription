e1f78e0150587de7897e659095fab63bf8a766cb
labohkip81 <labankiplagat81@gmail.com>
2019-11-16 14:57:16 +0300

    added the membership model.

--- video_service/memberships/models.py
+++ video_service/memberships/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+MEMBBERSHIP_CHOICES = (
+    ('Enterprise', 'ent'),
+    ('Professional', 'pro'),
+    ('Free', 'free')
+)
 # Create your models here.
+class Membership(models.Model):
+    slug = models.SlugField(),
+    membership_type = models.CharField(choices=MEMBBERSHIP_CHOICES, default='Free', max_length=30)
+    price = models.IntegerField(max_length=15, blank=True),
+    stripe_plan_id = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.membership_type
