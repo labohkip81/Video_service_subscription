d16880d64c8526a5a86a23812f6be4a7b36d1ad1
labohkip81 <labankiplagat81@gmail.com>
2019-11-17 11:19:11 +0300

    completed on the flow of the lesson detail view.

--- video_service/courses/models.py
+++ video_service/courses/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 from memberships.models import Membership
 # Create your models here.
@@ -12,6 +12,14 @@ class Course(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('courses:detail', kwargs={'slug': self.slug})
+    
+    @property
+    def lessons(self):
+        return self.lesson_set.all().order_by('position')
+
+
 
 class Lesson(models.Model):
     slug = models.SlugField()
@@ -23,3 +31,11 @@ class Lesson(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("courses:lesson_detail", 
+        kwargs={
+            "course_slug": self.course.slug, 
+            "lesson_slug": self.slug,
+        })
+    
