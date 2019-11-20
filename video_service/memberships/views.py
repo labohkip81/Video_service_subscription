87092cb6b45a8430285c2164ab4f9a2e9bb4b14c
labohkip81 <labankiplagat81@gmail.com>
2019-11-20 07:19:47 +0300

    url patterns

---
+++ video_service/memberships/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView
+
+from .models import Membership
+
+
+
+class MembershipSelectView(ListView):
+    model = Membership
--- video_service/video_service/urls.py
+++ video_service/video_service/urls.py
@@ -7,6 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('courses.urls', namespace='courses')),
+    path('', include('memberships.urls', namespace='courses'))
 ]
 
 if settings.DEBUG:
